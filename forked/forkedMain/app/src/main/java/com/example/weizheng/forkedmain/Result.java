5a1accfbe920d8660462362c8ea1efefd52960f4
fongwz <schifx95@gmail.com>
2017-07-07 12:21:57 +0800

    filter system

--- forked/forkedMain/app/src/main/java/com/example/weizheng/forkedmain/Result.java
+++ forked/forkedMain/app/src/main/java/com/example/weizheng/forkedmain/Result.java
@@ -8,6 +8,7 @@ import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,9 +18,12 @@ import com.google.firebase.database.ValueEventListener;
 public class Result extends AppCompatActivity {
 
     private Bundle data;
-    private int[] preferences;
+    private int[] firebasePreferences;
+    private int[] tempPreferences;
     private FirebaseDatabase myFirebaseDatabase;
     private DatabaseReference myFirebaseRef;
+    private FirebaseAuth myFirebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +35,32 @@ public class Result extends AppCompatActivity {
             return;
         }
 
-        preferences = data.getIntArray("preferences");
+        tempPreferences = data.getIntArray("preferences");
 
         myFirebaseDatabase = FirebaseDatabase.getInstance(); //reference to root directory
         myFirebaseRef = myFirebaseDatabase.getReference();
+        myFirebaseAuth = FirebaseAuth.getInstance();
+        firebasePreferences = new int[12];
+
+
+        /*** Obtains user preference values from FireBase database */
+        DatabaseReference preferenceReference = myFirebaseRef.child("Users").child(myFirebaseAuth.getCurrentUser().getUid()).child("Preferences");
+        preferenceReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int i = 0;
+
+                for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                    firebasePreferences[i] = (int) postSnapshot.getValue();
+                    i++;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         DatabaseReference recipesReference = myFirebaseRef.child("Recipe List");
         recipesReference.addValueEventListener(new ValueEventListener() {
@@ -43,9 +69,12 @@ public class Result extends AppCompatActivity {
                 int i=0;
                 String[] recipes = new String[5];
                 for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
-                    String name = postSnapshot.getKey();
-                    recipes[i]=name;
-                    i++;
+                    DataSnapshot categoryValues = postSnapshot.child("Categories");
+                    if(giveList(firebasePreferences, categoryValues)){
+                        String name = postSnapshot.getKey();
+                        recipes[i]=name;
+                        i++;
+                    }
                     if(i>4){break;}
                 }
                 ListAdapter adapter = new resultAdapter(getApplicationContext(), recipes);
@@ -61,5 +90,17 @@ public class Result extends AppCompatActivity {
 
     }
 
+    public boolean giveList(final int[] mPreferences, DataSnapshot categoryValues){
+        /** Take in preference values, filter according to result, and returns a boolean value to indicate whether recipe should be added to list */
 
+        int i = 0;
+        for(DataSnapshot postSnapshot : categoryValues.getChildren()){
+            if( postSnapshot.getValue() != mPreferences[i] ){
+                return false;
+            } else {
+                continue;
+            }
+        }
+        return true;
+    }
 }

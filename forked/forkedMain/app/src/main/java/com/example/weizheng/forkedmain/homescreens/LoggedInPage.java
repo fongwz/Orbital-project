cfe056a1bfd48fe64d17f57d9accd29c92aa3994
siri99 <kl_sirisha@yahoo.com>
2017-07-28 10:17:08 +0800

    blank

--- forked/forkedMain/app/src/main/java/com/example/weizheng/forkedmain/homescreens/LoggedInPage.java
+++ forked/forkedMain/app/src/main/java/com/example/weizheng/forkedmain/homescreens/LoggedInPage.java
@@ -47,7 +47,7 @@ public class LoggedInPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        deleteCache(this);
+
         setContentView(R.layout.homescreen_logged_in_page);
 
         data = getIntent().getExtras();
@@ -194,29 +194,6 @@ public class LoggedInPage extends AppCompatActivity {
         finish();
     }
 
-    public static void deleteCache(Context context) {
-        try {
-            File dir = context.getCacheDir();
-            deleteDir(dir);
-        } catch (Exception e) {}
-    }
-
-    public static boolean deleteDir(File dir) {
-        if (dir != null && dir.isDirectory()) {
-            String[] children = dir.list();
-            for (int i = 0; i < children.length; i++) {
-                boolean success = deleteDir(new File(dir, children[i]));
-                if (!success) {
-                    return false;
-                }
-            }
-            return dir.delete();
-        } else if(dir!= null && dir.isFile()) {
-            return dir.delete();
-        } else {
-            return false;
-        }
-    }
 
     /** Override back button functionality *****************************************/
     @Override
